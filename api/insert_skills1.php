0d88af1a51ebe774cfff4b7fe756af216fdad792
Developer 365Innovative <dev.365innovative@gmail.com>
2017-07-24 01:06:54 +0530

    test resume

--- api/insert_skills1.php
+++ api/insert_skills1.php
@@ -3,7 +3,6 @@
   include_once 'config.php';
 
   $page='insert_skills.php';
-
   if($_GET['ip'] && $_GET['name'] && $_GET['gender'] && $_GET['email'] && $_GET['city'] && $_GET['phone'] && $_GET['skills']) {
     try {
       $dbc = new PDO("mysql:host=$server;dbname=$db", $config['username'], $config['password']);
@@ -14,7 +13,7 @@
       $message = 'Connection Error';
       goto message;
     }
-
+    
     $name = $_GET['name'];
     $gender = $_GET['gender'];
     $email = $_GET['email'];
@@ -26,12 +25,12 @@
     $currentWorkLocation = $_GET['currentWorkLocation'];
     $skills = $_GET['skills'];
     $ip = $_GET['ip'];
+    $resume = 'N';
     $del_flg = 'N';
 
-    $insertNewResume = "INSERT INTO `resume`(`name`, `gender`, `email_id`, `city`, `mobile`, `job_function`, `exp_yr`, `exp_mon`, `current_work_location`, `key_skills`, `CRTD_DT`, `CRTD_IP`, `DEL_FLG`)
-     VALUES (:name,:gender,:email,:city,:phone,:jobFunction,:expYr,:expMon,:currentWorkLocation,:skills,NOW(3),:ip,:del_flg)";
+    $insertNewSkills = "INSERT INTO `resume`(`name`, `gender`, `email_id`, `city`, `mobile`, `job_function`, `exp_yr`, `exp_mon`, `current_work_location`, `key_skills`, `CRTD_DT`, `CRTD_IP`, `DEL_FLG`) VALUES (:name,:gender,:email,:city,:phone,:jobFunction,:expYr,:expMon,:currentWorkLocation,:skills,NOW(3),:ip,:del_flg)";
 
-    $query = $dbc->prepare($insertNewResume);
+    $query = $dbc->prepare($insertNewSkills);
     $query->bindParam(":name", $name);
     $query->bindParam(":gender", $gender);
     $query->bindParam(":email", $email);
@@ -42,6 +41,7 @@
     $query->bindParam(":expMon", $expMon);
     $query->bindParam(":currentWorkLocation", $currentWorkLocation);
     $query->bindParam(":skills", $skills);
+    // $query->bindParam(":resume", $resume);
     $query->bindParam(":ip", $ip);
     $query->bindParam(":del_flg", $del_flg);
 
@@ -52,26 +52,26 @@
       // Send mail
         $email_subject = "Your Enquiry - Reg";
 				$email_body = "<html><body>";
-				$email_body .= "Dear ".$person.",<br> We received your Resume, Thank you for contacting us. We will reach you soon regarding your query.<br><br><strong>Thanks and Regards</strong><br>ABC Private Limited";
+				$email_body .= "Dear ".$name.",<br> We received your resume, Thank you for contacting us. We will reach you soon regarding your query.<br><br><strong>Thanks and Regards</strong><br>ABC Private Limited";
 				$email_body .='</body></html>';
 				$headers = $config["from_email"]. "\r\n";
 				$headers .= "MIME-Version: 1.0\r\n";
 				$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-        $email_admin_body = "<html><body>";
-        $email_admin_body .= "Dear Sir,<br> We received a requirement from <strong>Mr.".$name."</strong>, The client requirement is as follows \n";
-        $email_admin_body .= "<table style='text-align:left;'><tr><th> <strong> Contact Name </strong> </th><td>".$name."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> Email Address </strong> </th><td>".$email."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> Phone </strong> </th><td>".$gender."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> Company Name </strong> </th><td>".$city."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> Sector </strong> </th><td>".$phone."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> City </strong> </th><td>".$jobFunction."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> Enquiry </strong> </th><td>".$currentWorkLocation."</td></tr>";
-        $email_admin_body .= "<tr><th> <strong> Address </strong> </th><td>".$skills."</td></tr>";
-
-        $email_admin_body .='</table></body></html>';
+        // $email_admin_body = "<html><body>";
+        // $email_admin_body .= "Dear Sir,<br> We received a resume from <strong>Mr.".$name."</strong>, The job seeker data is as follows \n";
+        // $email_admin_body .= "<table style='text-align:left;'><tr><th> <strong> Name </strong> </th><td>".$name."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Gender </strong> </th><td>".$gender."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Email </strong> </th><td>".$email."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> City </strong> </th><td>".$city."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Phone </strong> </th><td>".$phone."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Job Function </strong> </th><td>".$jobFunction."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Experience </strong> </th><td>".$expYr."years,".$expMon." months.</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Work Location </strong> </th><td>".$currentWorkLocation."</td></tr>";
+        // $email_admin_body .= "<tr><th> <strong> Skills </strong> </th><td>".$skills."</td></tr>";
+        // $email_admin_body .="</table></body></html>";
 
 				mail($email,$email_subject,$email_body,$headers);
-        mail($config['to_email'], $email_subject, $email_admin_body, $headers);
+        // mail($config['to_email'], $email_subject, $email_admin_body, $headers);
 
     } else {
       $status = 'N';
