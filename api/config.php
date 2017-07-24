30c3134e1bacb96bc01686ae4eeaead2ecd292bb
Developer 365Innovative <dev.365innovative@gmail.com>
2017-07-24 11:23:31 +0530

    fontface

--- api/config.php
+++ api/config.php
@@ -6,11 +6,11 @@
  $config["password"]= "S1P2A3R4K5@";
  // $config["password"]= "";
 
-  $config["from_email"] = 'From: Spark Consultancy <abctestpvtltd@gmail.com>';
-  $config["to_email"] = 'Reply-To: Spark Consultancy <abctestpvtltd@gmail.com>';
+  // $config["from_email"] = 'From: Spark Consultancy <abctestpvtltd@gmail.com>';
+  // $config["to_email"] = 'Reply-To: Spark Consultancy <abctestpvtltd@gmail.com>';
 
- // $config["from_email"] = 'From: Spark Consultancy <info@sparkconsultancyweb.com>';
- // $config["to_email"] = 'Reply-To: Spark Consultancy <info@sparkconsultancyweb.com>';
+ $config["from_email"] = 'From: Spark Consultancy <info@sparkconsultancyweb.com>';
+ $config["to_email"] = 'Reply-To: Spark Consultancy <info@sparkconsultancyweb.com>';
 
  $server = $config['host'];
  $db= $config['database'];
